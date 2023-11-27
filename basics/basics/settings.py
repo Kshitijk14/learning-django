bb7faae2803386772ecfff8f6c08d77db7ef75d6
Kshitij Chaturvedi <kshitijhrms@gmail.com>
2023-11-28 00:09:33 +0530

    Basic-Recipe-Webapp

--- basics/basics/settings.py
+++ basics/basics/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 EXTERNAL_APPS = [
     'home',
+    'vege',
 ]
 
 INSTALLED_APPS += EXTERNAL_APPS
@@ -121,7 +122,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
+# to make the images visible on the client side 
+import os
+
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIR = {
+    os.path.join(BASE_DIR, 'public/static')
+}
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
+MEDIA_URL = '/media/'
+# end of MEDIA_ROOT
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- basics/basics/urls.py
+++ basics/basics/urls.py
@@ -16,10 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
 from home.views import *
+from vege.views import *
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', home, name="home"),
+    
+    path('recipes/', recipes, name="recipes"),
+    path('delete-recipe/<id>/', delete_recipe, name="delete-recipe"),
+    path('update-recipe/<id>/', update_recipe, name="update-recipe"),
     
     path('about/', about, name="about"),
     path('contact/', contact, name="contact"),
@@ -28,3 +38,9 @@ urlpatterns = [
     
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                            document_root = settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
